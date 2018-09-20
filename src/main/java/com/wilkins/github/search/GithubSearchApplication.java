47379d5e9a4a04d1874d05f9da7e5a52c139d913
Robin Wilkins <robjwilkins@gmail.com>
2018-09-20 13:33:16 +0100

    initial project creation from spring initializr

---
+++ src/main/java/com/wilkins/github/search/GithubSearchApplication.java
@@ -0,0 +1,12 @@
+package com.wilkins.github.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubSearchApplication.class, args);
+    }
+}
